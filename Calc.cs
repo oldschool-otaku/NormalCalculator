c28dd901a59bb269d797d005166e680d2631b6a0
meow <90336027+oldschool-otaku@users.noreply.github.com>
2022-04-17 13:57:24 +0300

    init

---
+++ Calc.cs
@@ -0,0 +1,29 @@
+﻿namespace NormalCalculator;
+
+public class Calc
+{
+    private static int Plus(int a, int b) => a + b;
+    private static int Minus(int a, int b) => a - b;
+    private static int Divide(int a, int b)
+    {
+        try { return a / b; }
+        catch (DivideByZeroException) 
+        {
+            Console.WriteLine("Divide by zero!");
+            return 0;   
+        }
+    }
+    private static int Multiply(int a, int b) => a * b;
+    public static int Choise(int a, int b, char symb)
+    {
+        return symb switch
+        {
+            '+' => Plus(a, b),
+            '-' => Minus(a, b),
+            '*' => Multiply(a, b),
+            '/' => Divide(a, b),
+            _ => 0
+        };
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace NormalCalculator;
+
+internal class Program
+{
+    private static int a;
+    private static int b;
+    private static char symbol;
+    private static void Main()
+    {   
+        while (symbol != 0)
+        {
+            Console.WriteLine("Type first number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type symbol: ");
+            symbol = Convert.ToChar(Console.ReadLine() ?? throw new InvalidOperationException());
+
+            Console.WriteLine(Calc.Choise(a, b, symbol));
+        }
+    }
+}
